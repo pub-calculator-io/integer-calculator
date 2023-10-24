c7bfdbbc5b24470edeaa59a9e5196c28a12ce10a
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:13:03 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_integer_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Integer Calculator by Calculator.iO";
 
 function display_ci_integer_calculator(){
